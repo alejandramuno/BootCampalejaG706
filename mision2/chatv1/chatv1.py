a4489ba69fc4b140d6a4babdff21d6d6e7364657
alejandra <alejan1325@gmail.com>
2026-04-29 11:18:44 -0500

    29/04

---
+++ mision2/chatv1/chatv1.py
@@ -0,0 +1,8 @@
+from sklearn.feature_extraction.text import CountVectorizer
+# CountVectorizer convierte texto en vector
+ 
+from sklearn.naive_bayes import MultinomialNB
+"""
+MultinomalNB modelo de inteligencia 
+artificial que aprende relaciones entre texto y respuestas 
+"""
---
+++ mision2/chatv1/implementacion.md
@@ -0,0 +1,11 @@
+# guia de implementacion del chatbot(supervisado)
+
+import nltk
+nltk.download ('punkt')
+
+visual code
+python -m pip install nltk 
+
+intalar librerias
+
+``pip install scikit-learn nltk numpy gym==0.26.2 gym-notices``
